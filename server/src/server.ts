3f2313f66091a24f3fdbe98eefc258594da38c7c
נעם בן ברק <noam170193@gmail.com>
2024-07-20 11:00:50 +0300

    remove heroku condition

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,13 +29,6 @@ app.use(cors());
 app.use(express.json());
 
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
-}
-
 app.use('/scooters', scooterRouter);
 app.use('/users', userRouter); 
 app.use('/parkings', parkingRouter);
